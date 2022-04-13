c5d0e6681e36dc31f56fa37ff9dd1796dc6b2f0d
Msaksh <shakshammishra1997@gmail.com>
2022-04-13 13:03:26 +0530

    uploading 1st time

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_carousel"></div>
+            <div className="home_demandPartner"> </div>
+            <div className="home_benefits"></div>
+            <div className="home_merchant"></div>
+            <div className="home_categories"></div>
+            <div className="home_reblissians"></div>
+            <div className="home_ourTeams"></div>
+        </div>
+    );
+}
+
+export default Home;
